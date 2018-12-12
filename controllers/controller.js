e6db5164370202f0a40ffb021f5cc2131bac7589
Aaron McAdams <aaronmcadams@live.ie>
2018-12-12 16:43:59 +0000

    req.params

--- controllers/controller.js
+++ controllers/controller.js
@@ -58,7 +58,7 @@ exports.findOne = (req, res) => {
 
 // Find a single user via name
 exports.findName = (req, res) => {
-  const username = (req.path.split('/')[3]).toString()
+  const username = req.params.username.toLowerCase()
   User.find({"user.username": username})
     .then(user => {
         if(!user) {
@@ -82,7 +82,7 @@ exports.findName = (req, res) => {
 
 // Find a single user via email
 exports.findEmail = (req, res) => {
-  const email = (req.path.split('/')[3]).toString()
+  const email = const username = req.params.email.toLowerCase()
   User.find({"user.email": email})
     .then(user => {
         if(!user) {
